9be4ed59a3b09a19314dff09c8a51f495e03bcff
amir-android-dev <amir-hosseini@gmx.at>
2022-09-27 11:00:15 +0200

    Creating model for ROOM DB

--- app/src/main/java/com/amir/dogs/model/Model.kt
+++ app/src/main/java/com/amir/dogs/model/Model.kt
@@ -1,21 +1,40 @@
 package com.amir.dogs.model
 
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import com.google.gson.annotations.SerializedName
 
+@Entity
 data class DogBreed(
+    @ColumnInfo("breed_id")
     @SerializedName("id")
     val breedID: String?,
+
+    @ColumnInfo("dog_name")
     @SerializedName("name")
-    val dogBreed:String?,
+    val dogBreed: String?,
+
+    @ColumnInfo("life_span")
     @SerializedName("life_span")
     val lifeSpan: String?,
+
+    @ColumnInfo("breed_group")
     @SerializedName("breed_group")
     val breedGroup: String,
+
+    @ColumnInfo("bred_for")
     @SerializedName("bred_for")
     val bredFor: String?,
+
     @SerializedName("temperament")
     val temperament: String?,
+
+    @ColumnInfo("dog_url")
     @SerializedName("url")
     val imageUrl: String?
 
-)
+) {
+@PrimaryKey(autoGenerate = true)
+var uuid:Int = 0
+}
