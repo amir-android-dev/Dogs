788f39887a06dd2f69e0a3c1c858785179e4aa72
amir-android-dev <amir-hosseini@gmx.at>
2022-09-28 20:07:43 +0200

    Storing data in the database

--- app/src/main/java/com/amir/dogs/viewmodel/ListViewModel.kt
+++ app/src/main/java/com/amir/dogs/viewmodel/ListViewModel.kt
@@ -1,13 +1,18 @@
 package com.amir.dogs.viewmodel
+
+import android.app.Application
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.amir.dogs.model.DogApiService
 import com.amir.dogs.model.DogBreed
+import com.amir.dogs.model.DogDatabse
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.observers.DisposableSingleObserver
 import io.reactivex.schedulers.Schedulers
-class ListViewModel : ViewModel() {
+import kotlinx.coroutines.launch
+
+class ListViewModel(application: Application) : BaseViewModel(application) {
 
     private val dogService = DogApiService()
 
@@ -38,24 +43,44 @@ class ListViewModel : ViewModel() {
             //set the goal to be on separate thread(not an main thread)
             //Schedulers.newThread() it says i want this action be done in background thread
             dogService.getDogs().subscribeOn(Schedulers.newThread())
-                    //the result process of last action must be processed on the Main thread
-                    //in order to display it we need the main thread not background thread
+                //the result process of last action must be processed on the Main thread
+                //in order to display it we need the main thread not background thread
                 .observeOn(AndroidSchedulers.mainThread())
-                    //here we pass the observer
+                //here we pass the observer
                 .subscribeWith(object : DisposableSingleObserver<List<DogBreed>>() {
                     override fun onSuccess(dogList: List<DogBreed>) {
-                        dogs.value=dogList
-                        dogsLoadError.value = false
-                        loading.value = false
+                        storeDogsLocally(dogList)
                     }
 
                     override fun onError(e: Throwable) {
-                       dogsLoadError.value = true
+                        dogsLoadError.value = true
                         loading.value = false
                         e.printStackTrace()
                     }
                 })
         )
+    }
+
+    private fun dogsRetrieved(dogList: List<DogBreed>) {
+        dogs.value = dogList
+        dogsLoadError.value = false
+        loading.value = false
+    }
+
+    private fun storeDogsLocally(list: List<DogBreed>) {
+//coroutine are for us to access the database from seperate thread
+        //database cannot be access from the main thread
+        launch {
+            val dao = DogDatabse(getApplication()).dogDao()
+          dao.deleteAllDogs()
+            //*list.toTypedArray() : it gets a list and expand it to an individual element
+            val result = dao.insertAll(*list.toTypedArray())
+            var i = 0
+            while (i<list.size){
+                list[i].uuid = result[i].toInt()
+            }
+            dogsRetrieved(list)
+        }
     }
 
     //how to manage disposable to avoid memory leaks
